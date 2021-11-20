9d786f274903ed2c589db30c0a5e5df2f744ba40
allen <allenpip341@gmail.com>
2021-11-20 17:50:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
+const App = () => {
+  //STATE
+    const [list, setList] = useState(["laundry"])
+    const [inputText, setInputText] = useState("input")
+    
+    const addHandler = () => {
+      let storedList = [...list]
+      storedList.push(inputText)
+      setList(storedList)
+      
+   }
+  
+   const removeHandler = (index) => {
+     let storedList = [...list]
+     storedList.splice(index, 1)
+     setList(storedList)
+     
+   }
+  //  const resetInputfield = () => {
+  //    setInputText("");
+  //}
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="heading">ToDo List</h1>
+        {list.map((list, index) => {
+            return <h2 className="list" key={index}>{list}
+              <button className="buttons" onClick={() => removeHandler (index)}>delete one</button>
+            </h2>
+  
+        })}
+        <input className="buttons" type="text" onChange={(event) => setInputText(event.target.value)}/>
+  
+        <button className="buttons" onClick={addHandler}>add to my todo list</button>
+          {/* <input className="buttons" type="text" onInput={resetInputfield}/> */}
     </div>
-  );
-}
-
-export default App;
+  )
+  }
+  export default App
+  
